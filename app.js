cc7214559ad2828f202826d652c293f673996721
cnapolea <cjnapoleao@gmail.com>
2020-10-28 02:06:03 -0500

    Complete to do list server side (routes, get, and post request) and style website)

--- app.js
+++ app.js
@@ -7,9 +7,33 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
+let date = new Date(),
+dateOpts = {
+    weekday: 'long', 
+    year: 'numeric', 
+    month: 'long', 
+    day: 'numeric'
+},
+formatDate = date.toLocaleDateString('en-US', dateOpts);
+
+let tasks = [];
+
 
 app.get("/", (req, res) => {
-    res.render("index", {});
+    res.render("index", {date:formatDate, tasks:tasks});
+});
+
+app.post("/", (req, res) => {
+    
+    let task = req.body.taskInput;
+    
+    if (tasks.includes(task)) {
+        res.render("index", {date:formatDate, tasks:tasks});
+    } else {
+        tasks.push(task);
+        res.render("index", {date:formatDate, tasks:tasks});
+    }
+
 });
 
 
