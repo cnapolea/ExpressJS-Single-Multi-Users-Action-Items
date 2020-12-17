183a06e74b50625ac8f01f2fb74e15b4e9abd613
cnapolea <cjnapoleao@gmail.com>
2020-12-17 05:17:40 +0000

    add state to edit list name input.

--- client/src/componets/MainContainer/ListContainer/ListsDisplay/ActionListItem/ActionListItem.jsx
+++ client/src/componets/MainContainer/ListContainer/ListsDisplay/ActionListItem/ActionListItem.jsx
@@ -1,9 +1,10 @@
 import React, {useState, useReducer} from 'react';
-import {List, ListItem, ListItemText, ListItemSecondaryAction, IconButton} from '@material-ui/core/';
+import {List, ListItem, ListItemText, ListItemSecondaryAction, IconButton, Zoom} from '@material-ui/core/';
 
 import EditIcon from '@material-ui/icons/EditRounded';
 import DeleteIcon from '@material-ui/icons/DeleteRounded';
-import ConfirmIcon from '@material-ui/icons/CheckCircleOutlined';
+import ConfirmIcon from '@material-ui/icons/CheckCircle';
+import CancelIcon from '@material-ui/icons/CancelOutlined';
 
 import useStyles from './styles.js';
 
@@ -11,47 +12,63 @@ function ActionList(props){
 
     const classes = useStyles();
     
+    // Capture state change of edit input field
+    const [updatedListName, setListName] = useState();
+    
+    // Checks if the list selected was clicked or not
     const [listIsClicked, setListIsClicked] = useState();
     
     const [edit, setEditOn] =  useReducer((currentState, action) => {
+        // This reducer controls the state of the edit input field
         switch (action.type) {
             case 'EDIT_FIELD':
                 return {...currentState, btnClicked:true}
                 ;
-            case 'CANCE_EDIT': 
+            case 'CANCEL_EDIT': 
                 return {...currentState, btnClicked:false};
             case 'MAKE_UPDATE':
                 return {...currentState, btnClicked:false};
             default:
                 break;
         }
-    }, {editClicked:false});
+    }, {btnClicked:false});
     
     const handleListItemClick = () => {
         setListIsClicked(prev => !prev);
     };
 
     const editClickHandler = () => {
-        setListIsClicked({type:'EDIT_FIELD'})
-    }
+        setEditOn({type:'EDIT_FIELD'});
+    };
+
+    const cancelEditHandler = () => {
+        setEditOn({type:'CANCEL_EDIT'});
+    };
+
+    const confirmEditHandler = (e) => {
+        const [newName] = e.target.value;
+        setListName(newName);
+    };
 
     return(
         <div className={classes.root}>
             <List component="nav" aria-label="secondary mailbox folder">
-                <ListItem
-                button
-                selected={listIsClicked}
-                onClick={() => handleListItemClick()}>
-                    {edit.btnClicked?<input className={classes.editInput} name="editInput" type="text" placeholder={props.name} autoFocus/>:<ListItemText primary={props.name}/>}
+                <ListItem button selected={listIsClicked} onClick={handleListItemClick}>
+                    {edit.btnClicked?
+                        <input className={classes.editInput} name="editInput" type="text" placeholder={props.name} onChange={(e) => console.log(e.target.value)} value={updatedListName} autoFocus/>:
+                        <ListItemText primary={props.name}/>
+                    }
                     
-                    
-                    <ListItemSecondaryAction>
-                        <IconButton>
-                            <EditIcon/>
-                        </IconButton>
-                        <IconButton onClick={() => props.deleteHandler(props.name, props.id)}>
-                            <DeleteIcon/>
-                        </IconButton>
+                    <ListItemSecondaryAction>    
+                        {edit.btnClicked?
+                            <IconButton><ConfirmIcon/></IconButton>:
+                            <IconButton onClick={editClickHandler}><EditIcon/></IconButton>
+                        }
+                        {edit.btnClicked?
+                            <IconButton onClick={cancelEditHandler}><CancelIcon/></IconButton>:
+                            <IconButton onClick={() => props.deleteHandler(props.name, props.id)}><DeleteIcon/></IconButton>
+                        }
+                        
                     </ListItemSecondaryAction>
                 </ListItem>
             </List>
