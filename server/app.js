9680d6d8c36de327633c5754fe9e54f3747275ca
cnapolea <norahcruz@Eleonoras-MBP.home>
2020-12-20 11:12:02 +0000

    connect cloud db to server

--- server/app.js
+++ server/app.js
@@ -1,3 +1,5 @@
+//jshint esversion:6
+
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -10,19 +12,15 @@ import actionItems from './routes/actionItems.js';
 
 const app = express();
 
-
 app.use(cors());
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "http://localhost:3000")});
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
     
 // connection to local db
-const dbUri = 'mongodb://localhost/todolistDB' || process.env.REMOTE_DB_URI; 
+const DB_URL = process.env.DB_URL; 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
     .catch(err => console.log(err));
 
@@ -32,5 +30,3 @@ app.use('/api', actionItems);
 app.get('/', (req, res) => {
     res.send('Welcome to Nizzer\' API.');
 });
-
-// const dbUri = `mongodb+srv://${env.admin_username}:${env.admin_password}@cluster0.rpn4t.mongodb.net/todolistDB?retryWrites=true&w=majority`;
